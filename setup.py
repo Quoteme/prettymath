097b7b84dd13e55f5a7c8de8836526d1c549b755
Quoteme <lucahappel99@gmx.de>
2020-03-26 17:55:56 +0100

    changed from distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-from distutils.core import setup
+from setuptools import setup
 setup(name='prettymath',
       version='1.0',
       author='Luca Leon Happel',
       url='https://github.com/Quoteme/prettymath/',
-      packages=['prettymath'],
-      entry_points={"console_scripts": ["prettymath=prettymath.__main__:main"]},
+      py_modules=['prettymath'],
+      entry_points = {"console_scripts": ['prettymath = prettymath:main']},
       include_package_data=True,
       zip_safe=False
       )
