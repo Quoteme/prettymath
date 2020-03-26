b56362b557bc5cf0cbaaca377c56c3a4e611b613
Quoteme <lucahappel99@gmx.de>
2020-03-26 15:51:33 +0100

    added setup.py for installation using distutils

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+setup(name='prettymath',
+      version='1.0',
+      author='Luca Leon Happel',
+      url='https://github.com/Quoteme/prettymath/',
+      py_modules=['prettymath'],
+      )
